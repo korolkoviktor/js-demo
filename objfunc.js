90b01c740d3619eb859040a15cb0784c5cc1bc48
Viktor Korolko <vikingru3825@gmail.com>
2023-05-15 15:57:05 +0400

    update the code

--- objfunc.js
+++ objfunc.js
@@ -3,14 +3,11 @@ let car = {};
 car.color = 'black';
 
 //Change the color property of the car object to 'green'
-car.color = 'green'
+car.color = 'green';
 
 //Add the power property to the car object, which is a function and displays the engine power to the console
-car['power'] = enginePower();
-function enginePower() {
-  return 100;
-}
-console.log('Engine power = '+car.power);
+car['power'] = () => { return 100 };
+console.log('Engine power = ' + car.power());
 
 /*Pears and apples are accepted to the warehouse, write a function that returns the result 
 of adding the number of accepted pears and apples*/
@@ -23,15 +20,15 @@ function sumFruits(cart) {
   for (let i of Object.values(cart)) {
     sum += i;
   }
-  return console.log('Sum of pears and apples = '+ sum);
+  return console.log('Sum of pears and apples = ' + sum);
 };
 sumFruits(warehouse);
 
 /*Your name is saved in the payment terminal, write a function to define the name in the terminal 
 (if you entered your name, then hello + name, if not, then there is no such name)*/
 let namesarrow = ['John', 'Alex', 'Trevis'];
+
 function defineLogin(login) {
-  // let login = prompt('Enter your name');
   if (login == '') {
     console.log('Yor need to enter yor name!');
     return;
@@ -44,7 +41,8 @@ function defineLogin(login) {
   }
   console.log('There is no such name.');
 }
-console.log('The function which define the name:')
+console.log('The function which define the name:');
+
 defineLogin(); // empty name value
 defineLogin('Alex'); //enter a registered name
 defineLogin('Devid');// enter unregistered name
@@ -53,7 +51,7 @@ defineLogin('Devid');// enter unregistered name
 function showTypeofValue(arg) {
   return console.log('Type of argument is ' + typeof (arg));
 }
-showTypeofValue(NaN)
+showTypeofValue(NaN);
 
 //Write a function that determines whether a number is prime or not
 function deternPrimeNumber(num) {
