65116528803d6e0bffccd4cd92a4651dff8f37ec
Viktor Korolko <vikingru3825@gmail.com>
2023-05-24 13:51:33 +0400

    update code

--- classesprototypes.js
+++ classesprototypes.js
@@ -1,16 +1,17 @@
 /*Create 2 objects: animal and cat, add move property to animal object, 
 cat object must inherit move property from object animal*/
-let animal = {
-    smell: true,
-    hear: true,
-    watch: true
-};
-let cat = {
-    __proto__: animal,
-    sayMiau() {
-        console.log('Cat say Miau');
+class Animal {
+    constructor() {
+        this.smell=true;
+        this.hear=true;
+        this.watch=true;
     }
-};
+}
+
+const animal=new Animal();
+const cat = new Object();
+cat.__proto__=animal;
+
 animal.move = true; // add new property
 
 console.log(cat.move); //display result - cat inherit animal property
